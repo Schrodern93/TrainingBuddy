83b08dc4130bd9ae6c725724a84c8cb672ba49e9
Joakim Nikolai Schrøder <55440505+Schrodern93@users.noreply.github.com>
2022-03-16 15:26:08 +0100

    litt videre

--- QandAView.js
+++ QandAView.js
@@ -2,9 +2,26 @@ function QandAPageView(){
     let html = "";
 
     html =`<div class="QandAPageContainer">
-              noe skal inn her
+              <div class="qandaTittel">Q and A</div>
+            ${ showQandA()}
            </div>
     
             `;
     return html;
+}
+
+function showQandA(){
+let html = "";
+let qandALength = [1,2,3,4,5,6]; // må byttes ut slik at det er q and a
+for(let i = 0; i < qandALength.length;i++){
+
+    html += `<div class="qandaBox">
+    lots of questions
+    
+    </div>`
+
+}
+return html;
+
+
 }
